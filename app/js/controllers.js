ccda444a30b62abff2d706d41f1d55a8d06e8363
peterander <paanderson1@gmail.com>
2014-11-17 18:48:07 -0800

    ui-router

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,29 +2,45 @@
 
 /* Controllers */
 
-var portfolioApp = angular.module('portfolioApp', []);
-
-portfolioApp.controller('ArtCtrl', ['$scope', '$http',
-	function($scope, $http) {
+portfolioApp.controller('ArtCtrl', ['$scope', 'ArtService', '$http',
+	function($scope, ArtService, $http) {
 		$http.get('art/art.json').success(function(data) {
 			$scope.art = data;		
 	});
+	// $scope.art = ArtService.get();
+}]);
+
+portfolioApp.factory('ArtService', ['$http',
+	function($http){
+		return {
+			get: function(){
+				$http.get('art/art.json').success(function(data) {
+					return data;		
+				});
+			}
+		};
 }]);
 
 portfolioApp.controller('TravelsCtrl', function($scope) {
 	$scope.travels = [
 		{'name': 'China',
-		 'description': 'Visited in 2007. Stayed in Beijing.'},
+		 'description': 'Visited in 2007. Stayed in Beijing.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/1'},
 		{'name': 'Japan',
-		 'description': 'Visited in 2007. Stayed in Tokyo.'},
+		 'description': 'Visited in 2007. Stayed in Tokyo.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/2'},
 		{'name': 'England',
-		 'description': 'Visited as a kid. Went to London.'},
+		 'description': 'Visited as a kid. Went to London.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/3'},
 		{'name': 'France',
-		 'description': 'Visited in 2005. Toulouse and Paris.'},
+		 'description': 'Visited in 2005. Toulouse and Paris.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/4'},
 		{'name': 'Singapore',
-		 'description': 'Visited in 2014.'},
+		 'description': 'Visited in 2014. Went all over.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/5'},
 		{'name': 'India',
-		 'description': 'Visited in 2014. Tamil Nadu and Kerala.'},
+		 'description': 'Visited in 2014. Tamil Nadu and Kerala.',
+		 'imgUrl': 'http://lorempixel.com/64/64/city/6'},
 	];
 });
 
