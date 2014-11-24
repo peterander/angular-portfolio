ade10863dcdf1a2e43082b5d2abd75f253f96a71
peterander <paanderson1@gmail.com>
2014-11-24 08:09:08 -0800

    travel and riffs
    new travel thumbs and incomplete riff functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,24 +11,60 @@ portfolioApp.controller('ArtCtrl', ['$scope', '$http',
 
 portfolioApp.controller('TravelsCtrl', function($scope) {
 	$scope.travels = [
-		{'name': 'China',
+		{
+		 'name': 'China',
 		 'description': 'Visited in 2007. Stayed in Beijing.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/1'},
-		{'name': 'Japan',
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/1/',
+		 	'http://lorempixel.com/300/300/food/2/',
+		 	'http://lorempixel.com/300/300/food/3/'
+		 ]
+		},
+		{
+		 'name': 'Japan',
 		 'description': 'Visited in 2007. Stayed in Tokyo.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/2'},
-		{'name': 'England',
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/2/',
+		 	'http://lorempixel.com/300/300/food/3/',
+		 	'http://lorempixel.com/300/300/food/4/'
+		 ]
+		},
+		{
+		 'name': 'England',
 		 'description': 'Visited as a kid. Went to London.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/3'},
-		{'name': 'France',
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/3/',
+		 	'http://lorempixel.com/300/300/food/4/',
+		 	'http://lorempixel.com/300/300/food/5/'
+		 ]
+		},
+		{
+		 'name': 'France',
 		 'description': 'Visited in 2005. Toulouse and Paris.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/4'},
-		{'name': 'Singapore',
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/4/',
+		 	'http://lorempixel.com/300/300/food/5/',
+		 	'http://lorempixel.com/300/300/food/6/'
+		 ]
+		},
+		{
+		 'name': 'Singapore',
 		 'description': 'Visited in 2014. Went all over.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/5'},
-		{'name': 'India',
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/5/',
+		 	'http://lorempixel.com/300/300/food/6/',
+		 	'http://lorempixel.com/300/300/food/7/'
+		 ]
+		},
+		{
+		 'name': 'India',
 		 'description': 'Visited in 2014. Tamil Nadu and Kerala.',
-		 'imgUrl': 'http://lorempixel.com/64/64/city/6'},
+		 'gallery': [
+		 	'http://lorempixel.com/300/300/food/6/',
+		 	'http://lorempixel.com/300/300/food/7/',
+		 	'http://lorempixel.com/300/300/food/8/'
+		 ]
+		}
 	];
 });
 
@@ -46,42 +82,73 @@ portfolioApp.controller('RiffsCtrl', function($scope) {
 		riff.rank--;
 	};
 	$scope.predicate = "-rank";
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = 'riffs/camera-shutter-click-06.mp3';
+	$scope.playRiff = function(riff){
+		riff.isPlaying = true;
+		$scope.audio.play();
+	}
+	$scope.pauseRiff = function(riff){
+		riff.isPlaying = false;
+		$scope.audio.pause();
+	}
+	$scope.audio.addEventListener('ended', function() {
+		$scope.$apply(function() {
+			$scope.pauseRiff();
+		});
+	});
+    // $scope.isPlaying = false;
+    // $scope.audio = document.createElement('audio');
+    // $scope.audio.src = 'riffs/camera-shutter-click-06.mp3';
+    // $scope.playRiff = function(riff) {
+	   //  $scope.audio.play();
+	   //  $scope.isPlaying = true;
+    // };
+    // $scope.pauseRiff = function(riff) {
+	   //  $scope.audio.pause();
+	   //  $scope.isPlaying = false;
+    // };
+    // $scope.audio.addEventListener('ended', function() {
+	   //  $scope.$apply(function() {
+	   //    $scope.pauseRiff()
+	   //  });
+    // });
 	$scope.riffs = [
 		{'number': '1',
 		 'title': 'Wildwood Flower',
 		 'instrument': 'Banjo',
 		 'duration': '00:33',
-		 'url': 'camera-shutter-click-06.mp3',
+		 'url': 'riffs/camera-shutter-click-06.mp3',
 		 'rank': 0},
 		{'number': '2',
 		 'title': 'Picking',
 		 'instrument': '12-string',
 		 'duration': '00:27',
-		 'url': 'electric-drill-1.mp3',
+		 'url': 'riffs/electric-drill-1.mp3',
 		 'rank': 0},
 		{'number': '3',
 		 'title': 'Bendy',
 		 'instrument': 'Telecaster',
 		 'duration': '00:45',
-		 'url': 'electric-drill-2.mp3',
+		 'url': 'riffs/electric-drill-2.mp3',
 		 'rank': 0},
 		{'number': '4',
 		 'title': 'Clawhammer',
 		 'instrument': 'Banjo',
 		 'duration': '00:38',
-		 'url': 'freezer-hum-1.mp3',
+		 'url': 'riffs/freezer-hum-1.mp3',
 		 'rank': 0},
 		{'number': '5',
 		 'title': 'Open',
 		 'instrument': '12-string',
 		 'duration': '00:32',
-		 'url': 'grass-trimmer-1.mp3',
+		 'url': 'riffs/grass-trimmer-1.mp3',
 		 'rank': 0},
 		{'number': '6',
 		 'title': 'Hammer-on',
 		 'instrument': 'Telecaster',
 		 'duration': '00:19',
-		 'url': 'hammering-2.mp3',
+		 'url': 'riffs/hammering-2.mp3',
 		 'rank': 0},
 	];
 });
