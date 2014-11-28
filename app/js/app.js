b21e5c972acae5abcdb5e835f38acedcb80ddede
peterander <paanderson1@gmail.com>
2014-11-28 11:26:24 -0800

    Whitespace Commit

--- app/js/app.js
+++ app/js/app.js
@@ -5,44 +5,44 @@
 var portfolioApp = angular.module('portfolioApp', ['ui.router']);
 
 portfolioApp.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to this state
-  $urlRouterProvider.otherwise("/");
+    //
+    // For any unmatched url, redirect to this state
+    $urlRouterProvider.otherwise("/");
 
-  // Now set up the states
-  $stateProvider
-  	.state('art', {
-      url: "/art",
-      templateUrl: "partials/art.html"
-    })
-    .state('travels', {
-      url: "/travels",
-      templateUrl: "partials/travels.html"
-    })
-    .state('riffs', {
-      url: "/riffs",
-      templateUrl: "partials/riffs.html"
-    })
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "partials/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
-    });
+    // Now set up the states
+    $stateProvider
+      	.state('art', {
+          url: "/art",
+          templateUrl: "partials/art.html"
+        })
+        .state('travels', {
+          url: "/travels",
+          templateUrl: "partials/travels.html"
+        })
+        .state('riffs', {
+          url: "/riffs",
+          templateUrl: "partials/riffs.html"
+        })
+        .state('state1', {
+          url: "/state1",
+          templateUrl: "partials/state1.html"
+        })
+        .state('state1.list', {
+          url: "/list",
+          templateUrl: "partials/state1.list.html",
+          controller: function($scope) {
+            $scope.items = ["A", "List", "Of", "Items"];
+          }
+        })
+        .state('state2', {
+          url: "/state2",
+          templateUrl: "partials/state2.html"
+        })
+        .state('state2.list', {
+          url: "/list",
+          templateUrl: "partials/state2.list.html",
+          controller: function($scope) {
+            $scope.things = ["A", "Set", "Of", "Things"];
+          }
+        });
 });
